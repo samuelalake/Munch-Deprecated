baec5006d23cacfc9b8a8c22cf321d5ef2d08a88
samuelalake <harlahke@gmail.com>
2022-01-30 14:44:47 -0600

    Initial Commit

---
+++ Munch/MunchApp.swift
@@ -0,0 +1,17 @@
+//
+//  MunchApp.swift
+//  Munch
+//
+//  Created by Samuel Alake on 1/30/22.
+//
+
+import SwiftUI
+
+@main
+struct MunchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
